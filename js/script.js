ecde31f736a4591e95d70ba8a8fb67c558c51ae3
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2022-10-25 18:56:49 -1000

    finished party 3 of personal framework

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-// OVERLAY NAV MENU SHOW/HIDE //
+document.addEventListener('DOMContentLoaded', function () {
 
-const mymenubutton = document.querySelector('.menu-button');
-const mysitenav = document.querySelector('.site-header .site-nav');
+    // OVERLAY NAV MENU SHOW/HIDE
 
-mymenubutton.onclick = function () {
-    if ( mysitenav.getAttribute('data-navstate') === 'open') {
-    mysitenav.setAttribute('data-navstate', 'closed');
-    } else {
-    mysitenav.setAttribute('data-navstate', 'open');
+    const mymenubutton = document.querySelector('.menu-button');
+    const mysitenav = document.querySelector('.site-header .site-nav');
+
+        mymenubutton.onclick = function () {
+            if ( mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute('data-navstate', 'closed');
+        } else {
+        mysitenav.setAttribute('data-navstate', 'open');
+        }
+    };
+
+    // SCROLL TRIGGERED ANIMATION
+
+    let options = {
+        threshold: .25
     }
-}
+    const myobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.setAttribute("data-sectionstate", "active");
+            } else {
+                entry.target.setAttribute("data-sectionstate", "inactive");
+            }
+        });
+    }, options);
+
+    document.querySelectorAll(".scroll-triggered").forEach((el) => {
+        myobserver.observe(el);
+    });
+
+});
